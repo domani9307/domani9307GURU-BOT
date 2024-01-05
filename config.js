370ba186141467fc7c848054bd1839a56db79d3b
domani9307 <154927239+domani9307@users.noreply.github.com>
2024-01-05 23:09:34 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['+254739543193', 'me', true],
+  ['+254739543193', ', true], 
   [''] 
 ] //Number of owners
 
